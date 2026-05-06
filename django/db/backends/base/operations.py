74f6dce0512af9472a8bf8c108c8132f9728bfdd
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 15:26:20 +0000

    [RD-Bench V2] rdbench-v2/django__django-12741

--- django/db/backends/base/operations.py
+++ django/db/backends/base/operations.py
@@ -402,11 +402,10 @@ class BaseDatabaseOperations:
 
     def execute_sql_flush(self, using, sql_list):
         """Execute a list of SQL statements to flush the database."""
-        with transaction.atomic(using=using, savepoint=self.connection.features.can_rollback_ddl):
+        with transaction.atomic(using=self.connection.alias, savepoint=self.connection.features.can_rollback_ddl):
             with self.connection.cursor() as cursor:
                 for sql in sql_list:
                     cursor.execute(sql)
-
     def sequence_reset_by_name_sql(self, style, sequences):
         """
         Return a list of the SQL statements required to reset sequences
