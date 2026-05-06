2f5c1ff6eda4fff7c4b85c28aedd91a30a7eb9cb
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 16:04:56 +0000

    [RD-Bench V2] rdbench-v2/django__django-14376

--- django/db/backends/mysql/base.py
+++ django/db/backends/mysql/base.py
@@ -200,13 +200,12 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         if settings_dict['USER']:
             kwargs['user'] = settings_dict['USER']
         if settings_dict['NAME']:
-            kwargs['db'] = settings_dict['NAME']
+            kwargs['database'] = settings_dict['NAME']
         if settings_dict['PASSWORD']:
-            kwargs['passwd'] = settings_dict['PASSWORD']
+            kwargs['password'] = settings_dict['PASSWORD']
         if settings_dict['HOST'].startswith('/'):
             kwargs['unix_socket'] = settings_dict['HOST']
-        elif settings_dict['HOST']:
-            kwargs['host'] = settings_dict['HOST']
+        elif settings_dict['HOST']:            kwargs['host'] = settings_dict['HOST']
         if settings_dict['PORT']:
             kwargs['port'] = int(settings_dict['PORT'])
         # We need the number of potentially affected rows after an
--- tests/auth_tests/settings.py
+++ tests/auth_tests/settings.py
@@ -1,9 +1,39 @@
 import os
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [os.path.join(os.path.dirname(__file__), 'templates')],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
+}]
+
 AUTH_MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-]
+    'django.contrib.auth.middleware.AuthenticationMiddleware',]
 
 AUTH_TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
