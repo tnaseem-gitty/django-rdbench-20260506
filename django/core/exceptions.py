fdce7b537acd5c81490c6911a2f5be945e79719b
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 16:03:29 +0000

    [RD-Bench V2] rdbench-v2/django__django-13212

--- django/core/exceptions.py
+++ django/core/exceptions.py
@@ -98,28 +98,26 @@ NON_FIELD_ERRORS = '__all__'
 
 class ValidationError(Exception):
     """An error while validating data."""
-    def __init__(self, message, code=None, params=None):
+    def __init__(self, message, code=None, params=None, value=None):
         """
         The `message` argument can be a single error, a list of errors, or a
-        dictionary that maps field names to lists of errors. What we define as
-        an "error" can be either a simple string or an instance of
+        dictionary that maps field names to lists of errors. What we define as        an "error" can be either a simple string or an instance of
         ValidationError with its message attribute set, and what we define as
         list or dictionary can be an actual `list` or `dict` or an instance
         of ValidationError with its `error_list` or `error_dict` attribute set.
         """
         super().__init__(message, code, params)
+        self.value = value
 
         if isinstance(message, ValidationError):
-            if hasattr(message, 'error_dict'):
-                message = message.error_dict
+            if hasattr(message, 'error_dict'):                message = message.error_dict
             elif not hasattr(message, 'message'):
                 message = message.error_list
             else:
-                message, code, params = message.message, message.code, message.params
+                message, code, params, value = message.message, message.code, message.params, message.value
 
         if isinstance(message, dict):
-            self.error_dict = {}
-            for field, messages in message.items():
+            self.error_dict = {}            for field, messages in message.items():
                 if not isinstance(messages, ValidationError):
                     messages = ValidationError(messages)
                 self.error_dict[field] = messages.error_list
@@ -175,13 +173,51 @@ class ValidationError(Exception):
                 yield str(message)
 
     def __str__(self):
-        if hasattr(self, 'error_dict'):
-            return repr(dict(self))
-        return repr(list(self))
+        def __str__(self):
+            if hasattr(self, 'error_dict'):
+                return repr(dict(self))
+            print(f"Debug: value={self.value}")
+            return repr(list(self)) + f" (value: {self.value})"
 
     def __repr__(self):
         return 'ValidationError(%s)' % self
+    @property
+    def message_dict(self):
+        # Trigger an AttributeError if this ValidationError
+        # doesn't have an error_dict.
+        getattr(self, 'error_dict')
 
+        return dict(self)
+
+    @property
+    def messages(self):
+        if hasattr(self, 'error_dict'):
+            return sum(dict(self).values(), [])
+        return list(self)
+
+    def update_error_dict(self, error_dict):
+        if hasattr(self, 'error_dict'):
+            for field, error_list in self.error_dict.items():
+                error_dict.setdefault(field, []).extend(error_list)
+        else:
+            error_dict.setdefault(NON_FIELD_ERRORS, []).extend(self.error_list)
+        return error_dict
+
+    def __iter__(self):
+        if hasattr(self, 'error_dict'):
+            for field, errors in self.error_dict.items():
+                yield field, list(ValidationError(errors))
+        else:
+            for error in self.error_list:
+                message = error.message
+                if error.params:
+                    message %= error.params
+    def __str__(self):
+        if hasattr(self, 'error_dict'):
+            return repr(dict(self))
+        return repr(list(self)) + f" (value: {self.value})"
+    def __repr__(self):
+        return 'ValidationError(%s)' % self
 
 class EmptyResultSet(Exception):
     """A database query predicate is impossible."""
