bc389ea64f115b8406653a1443139e0be2b368da
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 15:26:09 +0000

    [RD-Bench V2] rdbench-v2/django__django-12308

---
+++ tests/settings.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'tests',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+SECRET_KEY = 'fake-key'
