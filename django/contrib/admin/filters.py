fac07ebfcd48e4883d6ce58d07d1407d0c643828
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 14:57:48 +0000

    [RD-Bench V2] rdbench-v2/django__django-11400

--- django/contrib/admin/filters.py
+++ django/contrib/admin/filters.py
@@ -194,10 +194,12 @@ class RelatedFieldListFilter(FieldListFilter):
         return [self.lookup_kwarg, self.lookup_kwarg_isnull]
 
     def field_choices(self, field, request, model_admin):
-        ordering = ()
+        ordering = None
         related_admin = model_admin.admin_site._registry.get(field.remote_field.model)
         if related_admin is not None:
             ordering = related_admin.get_ordering(request)
+        if ordering is None:
+            ordering = field.remote_field.model._meta.ordering or ()
         return field.get_choices(include_blank=False, ordering=ordering)
 
     def choices(self, changelist):
@@ -418,5 +420,26 @@ FieldListFilter.register(lambda f: True, AllValuesFieldListFilter)
 
 class RelatedOnlyFieldListFilter(RelatedFieldListFilter):
     def field_choices(self, field, request, model_admin):
-        pk_qs = model_admin.get_queryset(request).distinct().values_list('%s__pk' % self.field_path, flat=True)
-        return field.get_choices(include_blank=False, limit_choices_to={'pk__in': pk_qs})
+        ordering = None
+        related_admin = model_admin.admin_site._registry.get(field.remote_field.model)
+        if related_admin is not None:
+            ordering = related_admin.get_ordering(request)
+        if ordering is None:
+            ordering = field.remote_field.model._meta.ordering or ()
+        
+        limit_choices_to = field.get_limit_choices_to()
+        qs = field.remote_field.model._default_manager.complex_filter(limit_choices_to)
+        if ordering:
+            qs = qs.order_by(*ordering)
+
+        def get_nested_value(obj, path):
+            for part in path.split('__'):
+                obj = getattr(obj, part)
+            return obj
+
+        return [
+            (get_nested_value(x, self.field_path)._get_pk_val(), str(get_nested_value(x, self.field_path)))
+            for x in qs
+            if self.used_parameters.get(self.lookup_kwarg) == str(get_nested_value(x, self.field_path)._get_pk_val()) or
+               get_nested_value(x, self.field_path)._get_pk_val() in model_admin.get_queryset(request).values_list(self.field_path, flat=True)
+        ]
