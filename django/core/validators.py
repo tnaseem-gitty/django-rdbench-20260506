2c25cf9520b754b87ad14f5f71946761ed601c1e
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 14:56:50 +0000

    [RD-Bench V2] rdbench-v2/django__django-10097

--- django/core/validators.py
+++ django/core/validators.py
@@ -94,7 +94,7 @@ class URLValidator(RegexValidator):
 
     regex = _lazy_re_compile(
         r'^(?:[a-z0-9\.\-\+]*)://'  # scheme is validated separately
-        r'(?:\S+(?::\S*)?@)?'  # user:pass authentication
+        r'(?:(?:[^\s:@/]+(?::(?:[^\s:@/]|%[0-9A-Fa-f]{2})+)?)?@)?'  # user:pass authentication
         r'(?:' + ipv4_re + '|' + ipv6_re + '|' + host_re + ')'
         r'(?::\d{2,5})?'  # port
         r'(?:[/?#][^\s]*)?'  # resource path
