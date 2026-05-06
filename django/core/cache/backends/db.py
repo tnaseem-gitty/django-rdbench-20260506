a29934ef5f39ee9524fd265482b6c6750df3aecc
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 16:03:22 +0000

    [RD-Bench V2] rdbench-v2/django__django-13089

--- django/core/cache/backends/db.py
+++ django/core/cache/backends/db.py
@@ -267,13 +267,12 @@ class DatabaseCache(BaseDatabaseCache):
                 cursor.execute(
                     connection.ops.cache_key_culling_sql() % table,
                     [cull_num])
-                cursor.execute("DELETE FROM %s "
-                               "WHERE cache_key < %%s" % table,
-                               [cursor.fetchone()[0]])
+                result = cursor.fetchone()
+                if result is not None:
+                    cursor.execute("DELETE FROM %s WHERE cache_key < %%s" % table, [result[0]])
 
     def clear(self):
-        db = router.db_for_write(self.cache_model_class)
-        connection = connections[db]
+        db = router.db_for_write(self.cache_model_class)        connection = connections[db]
         table = connection.ops.quote_name(self._table)
         with connection.cursor() as cursor:
             cursor.execute('DELETE FROM %s' % table)
