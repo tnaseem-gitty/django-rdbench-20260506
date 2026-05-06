fe62b7b4dacaa59146bc692f422c9778053585ce
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 15:02:05 +0000

    [RD-Bench V2] rdbench-v2/django__django-14315

--- django/db/backends/postgresql/client.py
+++ django/db/backends/postgresql/client.py
@@ -51,8 +51,7 @@ class DatabaseClient(BaseDatabaseClient):
             env['PGSSLKEY'] = str(sslkey)
         if passfile:
             env['PGPASSFILE'] = str(passfile)
-        return args, env
-
+        return args, env if env else None
     def runshell(self, parameters):
         sigint_handler = signal.getsignal(signal.SIGINT)
         try:
