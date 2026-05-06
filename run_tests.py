1603f3a813a4ccff58c0cc10200e28040fe5e44a
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 16:07:13 +0000

    [RD-Bench V2] rdbench-v2/django__django-16661

---
+++ run_tests.py
@@ -0,0 +1,12 @@
+import os
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
+django.setup()
+TestRunner = get_runner(settings)
+test_runner = TestRunner()
+failures = test_runner.run_tests(['tests.modeladmin.tests'])
+if failures:
+    exit(1)
---
+++ test_settings.py
@@ -0,0 +1,46 @@
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'tests.modeladmin',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'tests.urls'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
