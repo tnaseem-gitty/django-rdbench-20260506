d21b2225dbe4beb08391f55ef222ef5564b2cef7
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 16:02:42 +0000

    [RD-Bench V2] rdbench-v2/django__django-11790

--- django/contrib/auth/forms.py
+++ django/contrib/auth/forms.py
@@ -165,11 +165,10 @@ class AuthenticationForm(forms.Form):
     Base class for authenticating users. Extend this to get a form that accepts
     username/password logins.
     """
-    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True}))
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'maxlength': 254}))
     password = forms.CharField(
         label=_("Password"),
-        strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
+        strip=False,        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
     )
 
     error_messages = {
