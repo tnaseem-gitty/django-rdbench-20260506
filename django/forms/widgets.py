9e3488fcc0f552819139138e45729b894e5227f7
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 14:59:04 +0000

    [RD-Bench V2] rdbench-v2/django__django-12276

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -387,6 +387,10 @@ class FileInput(Input):
     def value_omitted_from_data(self, data, files, name):
         return name not in files
 
+    def use_required_attribute(self, initial):
+        return super().use_required_attribute(initial) and not initial
+
+
 
 FILE_INPUT_CONTRADICTION = object()
 
@@ -451,8 +455,6 @@ class ClearableFileInput(FileInput):
             return False
         return upload
 
-    def use_required_attribute(self, initial):
-        return super().use_required_attribute(initial) and not initial
 
     def value_omitted_from_data(self, data, files, name):
         return (
