d092b2d6c3f76171c9295309628f31794edd4aec
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 16:01:42 +0000

    [RD-Bench V2] rdbench-v2/django__django-10973

--- tests/auth_tests/settings.py
+++ tests/auth_tests/settings.py
@@ -1,7 +1,8 @@
+SECRET_KEY = 'temporary_secret_key'
+
 import os
 
-AUTH_MIDDLEWARE = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
+AUTH_MIDDLEWARE = [    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
@@ -16,3 +17,18 @@ AUTH_TEMPLATES = [{
         ],
     },
 }]
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite3'),
+    }
+}
