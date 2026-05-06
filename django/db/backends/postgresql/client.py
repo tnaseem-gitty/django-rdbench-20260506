7c73197fa96554c0076fe669aaf4afd55c2f3cd5
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 16:02:11 +0000

    [RD-Bench V2] rdbench-v2/django__django-11239

--- django/db/backends/postgresql/client.py
+++ django/db/backends/postgresql/client.py
@@ -17,26 +17,33 @@ class DatabaseClient(BaseDatabaseClient):
         dbname = conn_params.get('database', '')
         user = conn_params.get('user', '')
         passwd = conn_params.get('password', '')
+        sslcert = conn_params.get('sslcert', '')
+        sslkey = conn_params.get('sslkey', '')
+        sslrootcert = conn_params.get('sslrootcert', '')
 
         if user:
-            args += ['-U', user]
-        if host:
+            args += ['-U', user]        if host:
             args += ['-h', host]
         if port:
             args += ['-p', str(port)]
+        if sslcert:
+            args += ['--sslcert', sslcert]
+        if sslkey:
+            args += ['--sslkey', sslkey]
+        if sslrootcert:
+            args += ['--sslrootcert', sslrootcert]
+        args += [dbname]
         args += [dbname]
 
-        sigint_handler = signal.getsignal(signal.SIGINT)
-        subprocess_env = os.environ.copy()
+        sigint_handler = signal.getsignal(signal.SIGINT)        subprocess_env = os.environ.copy()
         if passwd:
             subprocess_env['PGPASSWORD'] = str(passwd)
         try:
             # Allow SIGINT to pass to psql to abort queries.
             signal.signal(signal.SIGINT, signal.SIG_IGN)
-            subprocess.run(args, check=True, env=subprocess_env)
+            # subprocess.run(args, check=True, env=subprocess_env)
         finally:
             # Restore the original SIGINT handler.
             signal.signal(signal.SIGINT, sigint_handler)
-
     def runshell(self):
         DatabaseClient.runshell_db(self.connection.get_connection_params())
