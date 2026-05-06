ee3b96d08e096927eb770599d859684e79fbf973
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 14:58:05 +0000

    [RD-Bench V2] rdbench-v2/django__django-11603

--- django/db/models/aggregates.py
+++ django/db/models/aggregates.py
@@ -98,7 +98,7 @@ class Aggregate(Func):
 
 class Avg(FixDurationInputMixin, NumericOutputFieldMixin, Aggregate):
     function = 'AVG'
-    name = 'Avg'
+    allow_distinct = True
 
 
 class Count(Aggregate):
@@ -121,11 +121,13 @@ class Count(Aggregate):
 class Max(Aggregate):
     function = 'MAX'
     name = 'Max'
+    allow_distinct = True
 
 
 class Min(Aggregate):
     function = 'MIN'
     name = 'Min'
+    allow_distinct = True
 
 
 class StdDev(NumericOutputFieldMixin, Aggregate):
@@ -142,7 +144,7 @@ class StdDev(NumericOutputFieldMixin, Aggregate):
 class Sum(FixDurationInputMixin, Aggregate):
     function = 'SUM'
     name = 'Sum'
-
+    allow_distinct = True
 
 class Variance(NumericOutputFieldMixin, Aggregate):
     name = 'Variance'
