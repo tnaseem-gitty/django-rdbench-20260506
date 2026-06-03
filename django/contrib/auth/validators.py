f81a83ef83c6f2e33603d27b1619e135fdecff4e
RD-Bench V3 <rdbench@rubberduck.io>
2026-06-03 09:43:51 +0000

    [RD-Bench V3] rdbench-v3/django__django-11099

--- django/contrib/auth/validators.py
+++ django/contrib/auth/validators.py
@@ -7,19 +7,17 @@ from django.utils.translation import gettext_lazy as _
 
 @deconstructible
 class ASCIIUsernameValidator(validators.RegexValidator):
-    regex = r'^[\w.@+-]+$'
+    regex = r'\A[\w.@+-]+\Z'
     message = _(
         'Enter a valid username. This value may contain only English letters, '
-        'numbers, and @/./+/-/_ characters.'
-    )
+        'numbers, and @/./+/-/_ characters.'    )
     flags = re.ASCII
 
 
 @deconstructible
 class UnicodeUsernameValidator(validators.RegexValidator):
-    regex = r'^[\w.@+-]+$'
+    regex = r'\A[\w.@+-]+\Z'
     message = _(
         'Enter a valid username. This value may contain only letters, '
-        'numbers, and @/./+/-/_ characters.'
-    )
+        'numbers, and @/./+/-/_ characters.'    )
     flags = 0
